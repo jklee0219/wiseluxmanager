0b8b1776b3486ea43b14147d6a5983da42fb5039
Wonjae0128 <ssakga@gmail.com>
2024-07-02 10:07:13 +0900

    '테스트수정'

--- admn/application/views/goodsmove/list.php
+++ admn/application/views/goodsmove/list.php
@@ -99,6 +99,8 @@
                             <option value="seller" <?=($stype=='seller') ? "selected='selected'" : ""?>>판매자</option>
                             <option value="sellerphone" <?=($stype=='sellerphone') ? "selected='selected'" : ""?>>판매자연락처</option>
                   			<option value="modelname" <?=($stype=='modelname') ? "selected='selected'" : ""?>>모델명</option>
+                            <option value="shipplace" <?=($stype=='shipplace') ? "selected='selected'" : ""?>>발송지점</option>
+                            <option value="reciveplace" <?=($stype=='reciveplace') ? "selected='selected'" : ""?>>수령지점</option>
 							<option value="tb_purchase.note" <?=($stype=='tb_purchase.note') ? "selected='selected'" : ""?>>비고</option>
 						</select>
 						<input type="text" class="form-control input-sm" autocomplete="off" name="skeyword" value="<?=$skeyword?>" placeholder="키워드 검색">
