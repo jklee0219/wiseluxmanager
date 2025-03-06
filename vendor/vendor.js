3b604318dfcfae2036e07565cf7b1c7aae326cb4
Wonjae0128 <ssakga@gmail.com>
2025-03-06 21:03:23 +0900

    '수정'

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -109,9 +109,9 @@ function getPriceGuide(obj, price){
 	}else if(v <= 1000000){
 		price_guide1 = (v * 13) / 100;		
 	}else if(v <= 10000000){
-		price_guide1 = (v * 11) / 100;
+		price_guide1 = (v * 12) / 100;
 	}else{
-		price_guide1 = (v * 7) / 100;
+		price_guide1 = (v * 8) / 100;
 	}
 
 
